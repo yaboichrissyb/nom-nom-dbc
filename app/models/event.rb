df9a5cd8d184fb633da6ef956dbdc59af7817419
Sarah Finken-Hernandez & Chris Bunkers <pair+sfinkenhdz.yaboichrissyb@devbootcamp.com>
2016-03-18 11:54:05 -0500

    Add error messages, also a user can join an event

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,9 @@ class Event < ActiveRecord::Base
   belongs_to  :week
   has_many    :comments
   has_many    :meals
+  validates_presence_of :name, :start_date, :end_date, :max_people
+  validates :max_people, numericality: { only_integer: true }
+
 
   def is_in_the_future?
     result = start_date <=> Date::today
