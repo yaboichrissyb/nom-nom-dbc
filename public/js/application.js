1df035582b3f41e36abe46499bef72039aff1664
Chris Bunkers <christopher.bunkers@gmail.com>
2016-08-23 15:53:35 -0500

    fix api bug

--- public/js/application.js
+++ public/js/application.js
@@ -6,7 +6,7 @@ function MiniListing(response){
   this.imageUrl = response.imageUrlsBySize[90].replace(/=.*$/,"");
   this.ingredients = response.ingredients;
   this.id = response.id;
-};
+}
 
 function FullListing(response){
   this.responseUrl = response.source.sourceRecipeUrl;
@@ -15,7 +15,7 @@ function FullListing(response){
 }
 
 MiniListing.prototype.returnHtml=function(){
-  var html = ""
+  var html = "";
   html += "<a href ='" + this.id + "' class = 'recipe-link'><div class='recipe-listing'><div class = 'recipe-name'>" + this.name + "</div>";
   // html += "<img src='" + this.imageUrl + "' class = 'recipe-image' />";
   html += "<ul class='ingredients'>";
@@ -24,9 +24,9 @@ MiniListing.prototype.returnHtml=function(){
   // });
   html += "</ul></div></a>";
   return html;
-}
+};
 FullListing.prototype.returnHtml=function(){
-  var html = ""
+  var html = "";
   html += "<div data-id=" + this.listing_id + " class='listing'>";
   html += "<a href='" + this.url + "' target=_blank>";
   // html += "<img src='" + this.image_url + "' />";
@@ -34,17 +34,15 @@ FullListing.prototype.returnHtml=function(){
   html += "</a>";
   html += "</div>";
   return html;
-}
+};
 $(document).ready(function() {
-  //datepicker
   $(function() {
       $( ".datepicker" ).datepicker();
     });
-
-  $(".get-recipes").on("submit", function(event){
+  $(".get-recipes .add-event-form").on("click", function(event){
     event.preventDefault();
     $(".hidden-recipes").children().remove();
-    var baseURL = "http://api.yummly.com/v1/api/recipes?_app_id=8bf48580&_app_key=***************************";
+    var baseURL = "http://api.yummly.com/v1/api/recipes?_app_id=8bf48580&_app_key=8e399149c84faa2a335f632deecce2cb";
     var data = $(this).serialize();
     var url = baseURL + data;
     // $(".hidden-recipes").toggle();
@@ -56,33 +54,28 @@ $(document).ready(function() {
     ajaxRequest.done(function(response){
       // $(".hidden-recipes").text(response);
       var matches = response.matches;
-      console.log(response.matches)
       var html = "";
       for(var index = 0; index<matches.length; index++){
-
       var recipe = new MiniListing(matches[index]);
-      // var imageUrl = response.imageUrlsBySize[90].replace(/=.*$/,"");
-      // var ingredients = ""
-      // var ingredients += recipe.ingredients.toString().substring(0,25)
-      // html += "<div class = 'recipe-name'><a href='" + recipe.id +"' class = 'recipe-link'>" +recipe.recipeName+ "</a></div>"
-      // html += "<div class = 'recipe-name'>" + recipe.recipeName + "</div>"
-      // html += "<div class = 'ingredients'>" + ingredients + "</div>"
-      // html += "<div class = '"
+      var imageUrl = matches[index].imageUrlsBySize[90].replace(/=.*$/,"");
+      var ingredients = recipe.ingredients.toString().substring(0,25);
+      html += "<div class = 'recipe-name'><a href='" + recipe.id +"' class = 'recipe-link'>" +recipe.recipeName+ "</a></div>";
+      html += "<div class = 'recipe-name'>" + recipe.recipeName + "</div>";
+      html += "<div class = 'ingredients'>" + ingredients + "</div>";
+      html += "<div class = '";
       $(".hidden-recipes").append(recipe.returnHtml());
       $(".recipe-listing").last().css("background-image","url("+ recipe.imageUrl+")");
-      // $(".recipe-listing").last().css("background-repeat","no-repeat");
-      // $(".recipe-listing").last().css("background-size","cover");
+      $(".recipe-listing").last().css("background-repeat","no-repeat");
+      $(".recipe-listing").last().css("background-size","cover");
     }
-
-
-      // var data = response.attribution
+      var data = response.attribution;
     });
   });
 
   $(".hidden-recipes").on("click", ".recipe-link", function(event){
     event.preventDefault();
     var recipeId = $(this).attr("href");
-    var url = "http://api.yummly.com/v1/api/recipe/"+recipeId+"?_app_id=8bf48580&_app_key=*************************";
+    var url = "http://api.yummly.com/v1/api/recipe/"+recipeId+"?_app_id=8bf48580&_app_key=8e399149c84faa2a335f632deecce2cb";
     var ajaxRequest = $.ajax({
       method: "GET",
       url: url
@@ -94,7 +87,7 @@ $(document).ready(function() {
           win.focus();
       }else{
           alert('Please allow popups for this site');
-      };
+      }
     });
   });
 });
@@ -104,116 +97,3 @@ $(document).ready(function() {
 
 
 
-//   $(".post-container").on("click", ".vote-button", function(event){
-//     event.preventDefault();
-//     var url = $(this).children("a").attr("href")
-//     $that = $(this)
-//     $.ajax({
-//       method: "GET",
-//       url: url
-//     })
-//     .done(function(response){
-//     $that.children("a").css("color", "red")
-//     $that.parent().find(".points").html(response)
-//     $(".comment-points").html(response)
-//     });
-//   });
-
-//   $(".post-comments").on("click", ".comment-vote-button", function(event){
-//     event.preventDefault();
-//     var url = $(this).children("a").attr("href")
-//     $that = $(this)
-//     $.ajax({
-//       method: "GET",
-//       url: url
-//     })
-//     .done(function(response){
-//     $that.children("a").css("color", "red")
-//     });
-//   });
-
-//   $(".post-container").on("click", ".delete", function(event){
-//     event.preventDefault();
-//     var url = $(this).attr("href")
-//     $that = $(this)
-//     $.ajax({
-//       method: "DELETE",
-//       url: url
-//     })
-//     .done(function(response){
-//       $that.parents("#post"+response).remove()
-//     });
-//   });
-
-//   $("#comment").on("submit", function(event){
-//     event.preventDefault();
-//     $that = $(this)
-//     var currentCount = +$(".comments-count").html()
-//     var data = $(this).serialize()
-//     var url = $(this).attr("action")
-//     $.ajax({
-//       method: "POST",
-//       url: url,
-//       data: data
-//     })
-//     .done(function(response){
-//       $(".post-comments").append(response)
-//       $that.children("textarea").val("")
-//       $(".comments-count").html(currentCount+1)
-//     });
-//   });
-
-//   $("#posts").on("submit", function(event){
-//     event.preventDefault();
-//     $that = $(this)
-//     var data = $(this).serialize()
-//     var url = $(this).attr("action")
-//     $.ajax({
-//       method: "POST",
-//       url: url,
-//       data: data
-//     })
-//     .done(function(response){
-//       $("#posts-container").append(response)
-//       $that.children("input[name = 'title']").val("")
-//       $that.children("input[name = 'url']").val("")
-//     });
-//   });
-
-//   $("a[href='/sort/new']").on("click", function(){
-//     event.preventDefault();
-//     $.ajax({
-//       method: "GET",
-//       url: "/posts/new/sort"
-//     })
-//     .done(function(response){
-//       $("#posts-container").children().remove()
-//       $("#posts-container").append(response)
-//     });
-//   });
-
-//   $("a[href='/sort/comments']").on("click", function(){
-//     event.preventDefault();
-//     $.ajax({
-//       method: "GET",
-//       url: "/posts/comments/sort"
-//     })
-//     .done(function(response){
-//       $("#posts-container").children().remove()
-//       $("#posts-container").append(response)
-//     });
-//   });
-
-//   $("a[href='/sort/popular']").on("click", function(){
-//     event.preventDefault();
-//     $.ajax({
-//       method: "GET",
-//       url: "/posts/popular/sort"
-//     })
-//     .done(function(response){
-//       $("#posts-container").children().remove()
-//       $("#posts-container").append(response)
-//     });
-//   });
-
-// });
